d11e7c56f3ff2c6ce7d32c4dd0b73be5be82121b
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-10-29 20:39:54 +0330

    initialize app from express

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ dotenv.config({path: './config.env'});
 
 const crudRoute = require('./api/routes/crudRoutes');
 
+const app = express();
+const port = process.env.PORT;
+const server = app.listen(port, () => {
+    console.log(`App is running on http://localhost:${port}`);
+});
+
