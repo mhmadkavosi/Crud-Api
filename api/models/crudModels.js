84da2652007d64bbac6cdb65b67d6cd2f8d3c939
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-10-30 13:06:52 +0330

    update modle

--- api/models/crudModels.js
+++ api/models/crudModels.js
@@ -4,8 +4,7 @@ const taskShema = new mongoose.Schema({
     name:{
         type:String,
         required:[true,"Name is required!"],
-        maxlength:[120,"a name must less or equal then 120 chr"],
-        minlength:[40,"a name must less or equal then 40 chr"]
+        maxlength: [120, "a name must less or equal then 120 chr"],
     },
     sumery:{
         type:String,
